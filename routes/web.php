cf314dc3662415f0a4e7394dec7d73085bd5c455
adelbert00 <tazor1337@gmail.com>
2024-10-07 15:40:20 +0200

    part 1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+route::post('/students', [StudentController::class, 'store']);
+route::get('/students/{id}', [StudentController::class, 'show']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
